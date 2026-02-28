7881ad482b202682ce9f72993ee2fe654d6a6d1e
DHIVYA R <dhivya.rajannt@gmail.com>
2026-02-28 16:26:58 +0530

    Updated securityconfig

--- src/main/java/com/example/loan/configuration/SecurityConfig.java
+++ src/main/java/com/example/loan/configuration/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@SuppressWarnings("java:S4507")
 @Configuration
 public class SecurityConfig {
 
